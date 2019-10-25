9b81c110e70dd61baf8589dea10582c6679b6083
Alex Kalderimis <alex.kalderimis@gmail.com>
2019-10-25 15:44:07 +0100

    Avoid defining modules as namespaces
    We want deterministic autoloading, and the syntax:
    
    ```
    class DesignManagement::Foo
    end
    ```
    
    Provides it.

--- ee/app/models/design_management/design_at_version.rb
+++ ee/app/models/design_management/design_at_version.rb
@@ -1,139 +1,137 @@
 # frozen_string_literal: true
 
-module DesignManagement
-  # Tuple of design and version
-  # * has a composite ID, with lazy_find
-  class DesignAtVersion
-    include ActiveModel::Validations
+# Tuple of design and version
+# * has a composite ID, with lazy_find
+class DesignManagement::DesignAtVersion
+  include ActiveModel::Validations
 
-    attr_accessor :version
-    attr_accessor :design
+  attr_accessor :version
+  attr_accessor :design
 
-    validates_presence_of :version
-    validates_presence_of :design
+  validates_presence_of :version
+  validates_presence_of :design
 
-    def initialize(design: nil, version: nil)
-      @design, @version = design, version
-    end
+  def initialize(design: nil, version: nil)
+    @design, @version = design, version
+  end
 
-    def self.lazy_find(id)
-      BatchLoader.for(id).batch do |ids, found|
-        find_all.for_ids(ids).each do |record|
-          found[record.id, record]
-        end
+  def self.lazy_find(id)
+    BatchLoader.for(id).batch do |ids, found|
+      find_all.for_ids(ids).each do |record|
+        found[record.id, record]
       end
     end
+  end
 
-    def id
-      "#{design.id}.#{version.id}"
+  def id
+    "#{design.id}.#{version.id}"
+  end
+
+  def global_id
+    Gitlab::GlobalId.build(model_name: 'DesignManagement::DesignAtVersion', id: id)
+  end
+
+  def save!
+    validate!
+  end
+
+  def persisted?
+    design.persisted? && version.persisted?
+  end
+
+  def self.find_all
+    Query.new
+  end
+
+  # We can't fall back to ActiveRecord here and remain as efficient as we might like,
+  # since we want to avoid N+1 queries. By using Arel, we can always query for any number
+  # of designs-at-versions in 1 query, and load designs and versions from the same row.
+  #
+  # This guarantees:
+  #  * we only load valid designs-at-versions (i.e. those where the design and version
+  #    are on the same issue)
+  #  * we only load combinations that exist
+  #  * we don't load combinations that are not requested when ids are provided.
+  class Query
+    include Enumerable
+
+    attr_reader :query
+
+    def initialize
+      @query = Arel::SelectManager.new(designs_table)
+                  .join(versions_table)
+                  .on(share_issue)
     end
 
-    def global_id
-      Gitlab::GlobalId.build(model_name: 'DesignManagement::DesignAtVersion', id: id)
+    def each(&block)
+      to_records.each(&block)
     end
 
-    def save!
-      validate!
+    def for_ids(ids)
+      pairs = ids.map { |id| id.split('.').map(&:to_i) }
+
+      design_mentioned  = pairs_table[:design_id].eq(designs_table[:id])
+      version_mentioned = pairs_table[:version_id].eq(versions_table[:id])
+
+      cte = Arel::Nodes::As.new(pairs_table, select_pairs(pairs))
+
+      query.with(cte).join(pairs_table).on(design_mentioned.and(version_mentioned))
+
+      self
     end
 
-    def persisted?
-      design.persisted? && version.persisted?
+    def count
+      query.project(designs_table[:id].count)
+      res = ActiveRecord::Base.connection.exec_query(query.to_sql)
+      res.first['count']
     end
 
-    def self.find_all
-      Query.new
+    def empty?
+      count.zero?
     end
 
-    # We can't fall back to ActiveRecord here and remain as efficient as we might like,
-    # since we want to avoid N+1 queries. By using Arel, we can always query for any number
-    # of designs-at-versions in 1 query, and load designs and versions from the same row.
-    #
-    # This guarantees:
-    #  * we only load valid designs-at-versions (i.e. those where the design and version
-    #    are on the same issue)
-    #  * we only load combinations that exist
-    #  * we don't load combinations that are not requested when ids are provided.
-    class Query
-      include Enumerable
+    def to_records
+      query.project(designs_table[Arel.star], versions_table[Arel.star])
+      res = ActiveRecord::Base.connection.exec_query(query.to_sql)
 
-      attr_reader :query
+      _, n = res.columns.each.with_index.find { |c, i| c == 'id' && i > 0 }
+      d_cols = res.columns.take(n)
+      v_cols = res.columns.drop(n)
 
-      def initialize
-        @query = Arel::SelectManager.new(designs_table)
-                    .join(versions_table)
-                    .on(share_issue)
+      res.rows.map do |row|
+        d = Design.instantiate(d_cols.zip(row.take(n)).to_h)
+        v = Version.instantiate(v_cols.zip(row.drop(n)).to_h)
+        DesignAtVersion.new(design: d, version: v)
       end
+    end
+    alias_method :to_a, :to_records
 
-      def each(&block)
-        to_records.each(&block)
-      end
+    private :to_records
 
-      def for_ids(ids)
-        pairs = ids.map { |id| id.split('.').map(&:to_i) }
+    private
 
-        design_mentioned  = pairs_table[:design_id].eq(designs_table[:id])
-        version_mentioned = pairs_table[:version_id].eq(versions_table[:id])
+    def share_issue
+      designs_table[:issue_id].eq(versions_table[:issue_id])
+    end
 
-        cte = Arel::Nodes::As.new(pairs_table, select_pairs(pairs))
+    def designs_table
+      ::DesignManagement::Design.arel_table
+    end
 
-        query.with(cte).join(pairs_table).on(design_mentioned.and(version_mentioned))
+    def versions_table
+      ::DesignManagement::Version.arel_table
+    end
 
-        self
-      end
+    def pairs_table
+      Arel::Table.new('pairs')
+    end
 
-      def count
-        query.project(designs_table[:id].count)
-        res = ActiveRecord::Base.connection.exec_query(query.to_sql)
-        res.first['count']
-      end
+    def select_pairs(pairs)
+      pairs_table_def = Arel::Nodes::SqlLiteral.new('t (design_id, version_id)')
+      pairs_values = Arel::Nodes::Grouping.new(Arel::Nodes::ValuesList.new(pairs))
+      pairs_alias = Arel::Nodes::TableAlias.new(pairs_values, pairs_table_def)
 
-      def empty?
-        count.zero?
-      end
-
-      def to_records
-        query.project(designs_table[Arel.star], versions_table[Arel.star])
-        res = ActiveRecord::Base.connection.exec_query(query.to_sql)
-
-        _, n = res.columns.each.with_index.find { |c, i| c == 'id' && i > 0 }
-        d_cols = res.columns.take(n)
-        v_cols = res.columns.drop(n)
-
-        res.rows.map do |row|
-          d = Design.instantiate(d_cols.zip(row.take(n)).to_h)
-          v = Version.instantiate(v_cols.zip(row.drop(n)).to_h)
-          DesignAtVersion.new(design: d, version: v)
-        end
-      end
-      alias_method :to_a, :to_records
-
-      private :to_records
-
-      private
-
-      def share_issue
-        designs_table[:issue_id].eq(versions_table[:issue_id])
-      end
-
-      def designs_table
-        ::DesignManagement::Design.arel_table
-      end
-
-      def versions_table
-        ::DesignManagement::Version.arel_table
-      end
-
-      def pairs_table
-        Arel::Table.new('pairs')
-      end
-
-      def select_pairs(pairs)
-        pairs_table_def = Arel::Nodes::SqlLiteral.new('t (design_id, version_id)')
-        pairs_values = Arel::Nodes::Grouping.new(Arel::Nodes::ValuesList.new(pairs))
-        pairs_alias = Arel::Nodes::TableAlias.new(pairs_values, pairs_table_def)
-
-        Arel::SelectManager.new(pairs_alias).project(Arel.star)
-      end
+      Arel::SelectManager.new(pairs_alias).project(Arel.star)
     end
   end
 end
--- ee/app/models/design_management/version.rb
+++ ee/app/models/design_management/version.rb
@@ -1,107 +1,111 @@
 # frozen_string_literal: true
 
-module DesignManagement
-  class Version < ApplicationRecord
-    include Importable
-    include ShaAttribute
+class DesignManagement::Version < ApplicationRecord
+  include Importable
+  include ShaAttribute
 
-    NotSameIssue = Class.new(StandardError)
+  NotSameIssue = Class.new(StandardError)
 
-    class CouldNotCreateVersion < StandardError
-      attr_reader :sha, :issue_id, :actions
+  class CouldNotCreateVersion < StandardError
+    attr_reader :sha, :issue_id, :actions
 
-      def initialize(sha, issue_id, actions)
-        @sha, @issue_id, @actions = sha, issue_id, actions
-      end
-
-      def message
-        "could not create version from commit: #{sha}"
-      end
-
-      def sentry_extra_data
-        {
-          sha: sha,
-          issue_id: issue_id,
-          design_ids: actions.map { |a| a.design.id }
-        }
-      end
+    def initialize(sha, issue_id, actions)
+      @sha, @issue_id, @actions = sha, issue_id, actions
     end
 
-    belongs_to :issue
-    has_many :actions
-    has_many :designs,
-             through: :actions,
-             class_name: "DesignManagement::Design",
-             source: :design,
-             inverse_of: :versions
-
-    validates :designs, presence: true, unless: :importing?
-    validates :sha, presence: true
-    validates :sha, uniqueness: { case_sensitive: false, scope: :issue_id }
-    # We are not validating the issue object as it incurs an extra query to fetch
-    # the record from the DB. Instead, we rely on the foreign key constraint to
-    # ensure referential integrity.
-    validates :issue_id, presence: true, unless: :importing?
-
-    sha_attribute :sha
-
-    delegate :project, to: :issue
-
-    scope :for_designs, -> (designs) do
-      where(id: Action.where(design_id: designs).select(:version_id)).distinct
-    end
-    scope :earlier_or_equal_to, -> (version) { where('id <= ?', version) }
-    scope :ordered, -> { order(id: :desc) }
-    scope :for_issue, -> (issue) { where(issue: issue) }
-
-    # This is the one true way to create a Version.
-    #
-    # This method means you can avoid the paradox of versions being invalid without
-    # designs, and not being able to add designs without a saved version. Also this
-    # method inserts designs in bulk, rather than one by one.
-    #
-    # Parameters:
-    # - designs [DesignManagement::DesignAction]:
-    #     the actions that have been performed in the repository.
-    # - sha [String]:
-    #     the SHA of the commit that performed them
-    # returns [DesignManagement::Version]
-    def self.create_for_designs(design_actions, sha)
-      issue_id, not_uniq = design_actions.map(&:issue_id).compact.uniq
-      raise NotSameIssue, 'All designs must belong to the same issue!' if not_uniq
-
-      transaction do
-        version = safe_find_or_create_by(sha: sha, issue_id: issue_id)
-        version.save(validate: false) # We need it to have an ID, validate later
-
-        rows = design_actions.map { |action| action.row_attrs(version) }
-
-        Gitlab::Database.bulk_insert(Action.table_name, rows)
-        version.designs.reset
-        version.validate!
-        design_actions.each(&:performed)
-
-        version
-      end
-    rescue
-      raise CouldNotCreateVersion.new(sha, issue_id, design_actions)
+    def message
+      "could not create version from commit: #{sha}"
     end
 
-    def designs_by_event
-      actions
-        .includes(:design)
-        .group_by(&:event)
-        .transform_values { |group| group.map(&:design) }
+    def sentry_extra_data
+      {
+        sha: sha,
+        issue_id: issue_id,
+        design_ids: actions.map { |a| a.design.id }
+      }
     end
+  end
 
-    def author
-      commit&.author
+  belongs_to :issue
+  has_many :actions
+  has_many :designs,
+           through: :actions,
+           class_name: "DesignManagement::Design",
+           source: :design,
+           inverse_of: :versions
+
+  validates :designs, presence: true, unless: :importing?
+  validates :sha, presence: true
+  validates :sha, uniqueness: { case_sensitive: false, scope: :issue_id }
+  # We are not validating the issue object as it incurs an extra query to fetch
+  # the record from the DB. Instead, we rely on the foreign key constraint to
+  # ensure referential integrity.
+  validates :issue_id, presence: true, unless: :importing?
+
+  sha_attribute :sha
+
+  delegate :project, to: :issue
+
+  scope :for_designs, -> (designs) do
+    where(id: Action.where(design_id: designs).select(:version_id)).distinct
+  end
+  scope :earlier_or_equal_to, -> (version) { where('id <= ?', version) }
+  scope :ordered, -> { order(id: :desc) }
+  scope :for_issue, -> (issue) { where(issue: issue) }
+
+  # This is the one true way to create a Version.
+  #
+  # This method means you can avoid the paradox of versions being invalid without
+  # designs, and not being able to add designs without a saved version. Also this
+  # method inserts designs in bulk, rather than one by one.
+  #
+  # Parameters:
+  # - designs [DesignManagement::DesignAction]:
+  #     the actions that have been performed in the repository.
+  # - sha [String]:
+  #     the SHA of the commit that performed them
+  # returns [DesignManagement::Version]
+  def self.create_for_designs(design_actions, sha)
+    issue_id, not_uniq = design_actions.map(&:issue_id).compact.uniq
+    raise NotSameIssue, 'All designs must belong to the same issue!' if not_uniq
+
+    transaction do
+      version = safe_find_or_create_by(sha: sha, issue_id: issue_id)
+      version.save(validate: false) # We need it to have an ID, validate later
+
+      rows = design_actions.map { |action| action.row_attrs(version) }
+
+      Gitlab::Database.bulk_insert(DesignManagement::Action.table_name, rows)
+      version.designs.reset
+      version.validate!
+      design_actions.each(&:performed)
+
+      version
     end
+  rescue
+    raise CouldNotCreateVersion.new(sha, issue_id, design_actions)
+  end
 
-    private
+  def designs_by_event
+    actions
+      .includes(:design)
+      .group_by(&:event)
+      .transform_values { |group| group.map(&:design) }
+  end
 
-    def commit
-      @commit ||= issue.project.design_repository.commit(sha)
+  def author
+    commit&.author
+  end
+
+  def diff_refs
+    strong_memoize(:diff_refs) do
+      sha && issue.project.design_repository.commit(sha).diff_refs
     end
+  end
+
+  private
+
+  def commit
+    @commit ||= issue.project.design_repository.commit(sha)
   end
 end
