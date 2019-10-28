038cf114221ea55d45c1bb998bd5a81244d658a4
Alex Kalderimis <alex.kalderimis@gmail.com>
2019-10-28 18:30:07 +0000

    Add new fields to spec

--- ee/spec/graphql/types/design_management/version_type_spec.rb
+++ ee/spec/graphql/types/design_management/version_type_spec.rb
@@ -5,5 +5,5 @@ require 'spec_helper'
 describe GitlabSchema.types['DesignVersion'] do
   it { expect(described_class).to require_graphql_authorizations(:read_design) }
 
-  it { expect(described_class).to have_graphql_fields(:id, :sha, :designs) }
+  it { expect(described_class).to have_graphql_fields(:id, :sha, :designs, :design_at_version, :designs_at_version) }
 end
