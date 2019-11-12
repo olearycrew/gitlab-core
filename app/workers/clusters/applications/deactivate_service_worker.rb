97cc7af901d0807e98dcaddd9ac326d730ace129
Mikolaj Wawrzyniak <mwawrzyniak@gitlab.com>
2019-11-12 15:11:44 +0100

    Add DeactivateServiceWorker for Cluster Apps
    Instance level installed apps can have big number of services attached,
    to make sure operation will not time out, it has to be moved to
    background.

---
+++ app/workers/clusters/applications/deactivate_service_worker.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+module Clusters
+  module Applications
+    class DeactivateServiceWorker
+      include ApplicationWorker
+      include ClusterQueue
+
+      def perform(cluster_id, service_name)
+        service = "#{service_name}_service".to_sym
+
+        Clusters::Cluster.find(cluster_id).try do |cluster|
+          cluster.all_projects.with_service(service).find_each do |project|
+            project.public_send(service).update!(active: false) # rubocop:disable GitlabSecurity/PublicSend
+          end
+        end
+      end
+    end
+  end
+end
