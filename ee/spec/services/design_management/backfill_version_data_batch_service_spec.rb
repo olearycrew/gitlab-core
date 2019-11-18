nickbrandt/GitLab

Capture author and created_at data for Version
https://gitlab.com/gitlab-org/gitlab/merge_requests/17316 introduced
new user_id and created_at columns for `Versions`
https://gitlab.com/gitlab-org/gitlab/merge_requests/18506 renamed this
column to author_id.

This MR has the app capture a `author_id` when new versions are created.

It also contains a post-migration to backfill author and created_at data
for `Version`s by sourcing from `Commit`s.

https://gitlab.com/gitlab-org/gitlab/issues/32081
https://gitlab.com/gitlab-org/gitlab/issues/13426

ee/spec/services/design_management/backfill_version_data_batch_service_spec.rb
@@ -1,0 +1,60 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe DesignManagement::BackfillVersionDataBatchService do
+  describe '#execute' do
+    set(:issue) { create(:issue) }
+    set(:project) { issue.project }
+
+    subject { execute_service }
+
+    def execute_service
+      described_class.new(issue.id).execute
+    end
+
+    def create_versions_with_missing_data(num_records: 1)
+      designs = create_list(:design, num_records, :with_file, issue: issue)
+      DesignManagement::Version.update_all(author_id: nil, created_at: nil)
+
+      designs.flat_map(&:versions)
+    end
+
+    it 'updates versions with the correct data' do
+      version = create_versions_with_missing_data.first
+      commit = project.design_repository.commit
+
+      expect(commit.author).to be_present
+      expect(commit.created_at).to be_present
+      expect { subject }.to(
+        change do
+          version.reload
+          [version.author_id, version.created_at]
+        end
+        .from([nil, nil])
+        .to([commit.author.id, commit.created_at])
+      )
+    end
+
+    it 'logs an error if commit cannot be found' do
+      create_versions_with_missing_data
+
+      expect_next_instance_of(DesignManagement::Repository) do |repo|
+        expect(repo).to receive(:commits_by).and_return([])
+      end
+      expect(Gitlab::BackgroundMigration::Logger).to receive(:error).once.with(
+        a_hash_including(message: 'No commit found for version')
+      )
+
+      subject
+    end
+
+    it 'loads all User records in a single batch' do
+      create_versions_with_missing_data(num_records: 10)
+
+      expect(User).to receive(:where).once.and_call_original
+
+      subject
+    end
+  end
+end

ee/spec/services/design_management/backfill_version_data_service_spec.rb
@@ -1,0 +1,54 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe DesignManagement::BackfillVersionDataService do
+  describe '.execute' do
+    set(:author) { create(:user) }
+    let(:batching_service) { DesignManagement::BackfillVersionDataBatchService }
+
+    subject { described_class.execute }
+
+    it 'selects versions with missing author_id' do
+      version = create(:design_version)
+      version.update_column(:author_id, nil)
+
+      expect(batching_service).to receive(:new).once.and_call_original
+
+      subject
+    end
+
+    it 'selects versions with missing created_at' do
+      version = create(:design_version, author: author)
+      version.update_column(:created_at, nil)
+
+      expect(batching_service).to receive(:new).once.and_call_original
+
+      subject
+    end
+
+    it 'does not select versions with author_id and created_at' do
+      version = create(:design_version, author: author)
+
+      expect(version.author_id).not_to be_nil
+      expect(version.created_at).not_to be_nil
+      expect(batching_service).not_to receive(:new)
+
+      subject
+    end
+
+    it 'batches per issue' do
+      # Create 3 versions in 2 issues
+      issue_1, issue_2 = create_list(:issue, 2)
+      create_list(:design_version, 2, issue: issue_1)
+      create(:design_version, issue: issue_2)
+
+      ::DesignManagement::Version.update_all(author_id: nil)
+
+      expect(batching_service).to receive(:new).once.with(issue_1.id).and_call_original
+      expect(batching_service).to receive(:new).once.with(issue_2.id).and_call_original
+
+      subject
+    end
+  end
+end

olearycrew/gitlab-core

ee/spec/services/design_management/backfill_version_data_batch_service_spec.rb
@@ -1,0 +1,71 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe DesignManagement::BackfillVersionDataBatchService do
+  describe '#execute' do
+    let_it_be(:issue) { create(:issue) }
+    let_it_be(:project) { issue.project }
+
+    subject { execute_service }
+
+    def execute_service
+      described_class.new(issue.id).execute
+    end
+
+    def create_versions_with_missing_data(num_records: 1)
+      designs = create_list(:design, num_records, :with_file, issue: issue)
+      DesignManagement::Version.update_all(author_id: nil, created_at: nil)
+
+      designs.flat_map(&:versions)
+    end
+
+    it 'updates versions with the correct data' do
+      version = create_versions_with_missing_data.first
+      commit = project.design_repository.commit
+
+      expect(commit.author).to be_present
+      expect(commit.created_at).to be_present
+      expect { subject }.to(
+        change do
+          version.reload
+          [version.author_id, version.created_at]
+        end
+        .from([nil, nil])
+        .to([commit.author.id, commit.created_at])
+      )
+    end
+
+    it 'leaves versions in a valid state' do
+      version = create_versions_with_missing_data.first
+
+      expect(version).to be_valid
+      expect { subject }.not_to(
+        change do
+          version.reload.valid?
+        end
+      )
+    end
+
+    it 'logs an error if commit cannot be found' do
+      create_versions_with_missing_data
+
+      expect_next_instance_of(DesignManagement::Repository) do |repo|
+        expect(repo).to receive(:commits_by).and_return([])
+      end
+      expect(Gitlab::BackgroundMigration::Logger).to receive(:error).once.with(
+        a_hash_including(message: 'No commit found for version')
+      )
+
+      subject
+    end
+
+    it 'loads all User records in a single batch' do
+      create_versions_with_missing_data(num_records: 10)
+
+      expect(User).to receive(:where).once.and_call_original
+
+      subject
+    end
+  end
+end

ee/spec/services/design_management/backfill_version_data_service_spec.rb
@@ -1,0 +1,54 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe DesignManagement::BackfillVersionDataService do
+  describe '.execute' do
+    let_it_be(:author) { create(:user) }
+    let_it_be(:batching_service) { DesignManagement::BackfillVersionDataBatchService }
+
+    subject { described_class.execute }
+
+    it 'selects versions with missing author_id' do
+      version = create(:design_version)
+      version.update_column(:author_id, nil)
+
+      expect(batching_service).to receive(:new).once.and_call_original
+
+      subject
+    end
+
+    it 'selects versions with missing created_at' do
+      version = create(:design_version, author: author)
+      version.update_column(:created_at, nil)
+
+      expect(batching_service).to receive(:new).once.and_call_original
+
+      subject
+    end
+
+    it 'does not select versions with author_id and created_at' do
+      version = create(:design_version, author: author)
+
+      expect(version.author_id).not_to be_nil
+      expect(version.created_at).not_to be_nil
+      expect(batching_service).not_to receive(:new)
+
+      subject
+    end
+
+    it 'batches per issue' do
+      # Create 3 versions in 2 issues
+      issue_1, issue_2 = create_list(:issue, 2)
+      create_list(:design_version, 2, issue: issue_1)
+      create(:design_version, issue: issue_2)
+
+      ::DesignManagement::Version.update_all(author_id: nil)
+
+      expect(batching_service).to receive(:new).once.with(issue_1.id).and_call_original
+      expect(batching_service).to receive(:new).once.with(issue_2.id).and_call_original
+
+      subject
+    end
+  end
+end