781008925d279cb4dde745879bf1f8f7deb2f035
Mikolaj Wawrzyniak <mwawrzyniak@gitlab.com>
2019-11-26 15:56:47 +0100

    Use custom error message

--- app/workers/clusters/applications/deactivate_service_worker.rb
+++ app/workers/clusters/applications/deactivate_service_worker.rb
@@ -8,9 +8,16 @@ module Clusters
 
       def perform(cluster_id, service_name)
         service = "#{service_name}_service".to_sym
-        Clusters::Cluster.find(cluster_id).all_projects.with_service(service).find_each do |project|
+        cluster = Clusters::Cluster.find_by_id(cluster_id)
+        raise cluster_missing_error(service_name) unless cluster
+
+        cluster.all_projects.with_service(service).find_each do |project|
           project.public_send(service).update!(active: false) # rubocop:disable GitlabSecurity/PublicSend
         end
+      end
+
+      def cluster_missing_error(service)
+        ActiveRecord::RecordNotFound.new("Can't deactivate #{service} services, host cluster not found! Some inconsistent records may left in database.")
       end
     end
   end
