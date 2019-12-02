79ba795f52af064f886f934bc2e3fa082adcfda8
Dave Pisek <dpisek@gitlab.com>
2019-12-02 17:26:32 +1100

    Add default export to getters file

--- ee/app/assets/javascripts/security_dashboard/store/modules/vulnerable_projects/getters.js
+++ ee/app/assets/javascripts/security_dashboard/store/modules/vulnerable_projects/getters.js
@@ -1,4 +1,5 @@
 import { groupBySeverityLevel } from 'ee/security_dashboard/store/modules/vulnerable_projects/utils';
 
-// eslint-disable-next-line import/prefer-default-export
 export const severityGroups = ({ projects }) => groupBySeverityLevel(projects);
+
+export default () => {};
