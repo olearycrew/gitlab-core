fd199438a6d59086da79bbd1f35f6db12dacd7e0
David Fernandez <dfernandez@gitlab.com>
2019-12-02 09:42:42 +0100

    Use OR form Remove useless quote statements Raise error if above the rows limit earlier

--- ee/app/models/packages/dependency.rb
+++ ee/app/models/packages/dependency.rb
@@ -8,25 +8,21 @@ class Packages::Dependency < ApplicationRecord
   validates :name,
     uniqueness: { scope: %i[package_id version_pattern] }
 
+  NAME_VERSION_PATTERN_TUPLE_MATCHING = '(name, version_pattern) = (?, ?)'.freeze
+
   def self.for_names_and_version_patterns(names = [], version_patterns = [], chunk_size = 50, max_rows_limit = 200)
     raise ArgumentError, "Parameters sizes don't match" unless names.size == version_patterns.size
 
-    sanitized_names = names.map { |n| connection.quote(n) }
-    sanitized_version_patterns = version_patterns.map { |n| connection.quote(n) }
-    columns = "(#{connection.quote_column_name(:name)}, #{connection.quote_column_name(:version_pattern)})"
     matched_ids = []
-    sanitized_names.zip(sanitized_version_patterns).each_slice(chunk_size) do |tuples|
-      where_statement = <<-EOF
-        #{columns} IN
-        (#{tuples.map { |tuple| "(#{tuple.join(', ')})" }.join(', ')})
-      EOF
-      matched_ids << where(where_statement).pluck(:id)
+    names.zip(version_patterns).each_slice(chunk_size) do |tuples|
+      where_statement = Array.new(tuples.size, NAME_VERSION_PATTERN_TUPLE_MATCHING)
+                             .join(' OR ')
+      matched_ids.concat(where(where_statement, *tuples.flatten).pluck(:id))
+
+      raise ArgumentError, "Parameters select too many Dependencies" if matched_ids.size > max_rows_limit
     end
 
-    matched_ids.flatten!
     return none if matched_ids.empty?
-
-    raise ArgumentError, "Parameters select too many Dependencies" if matched_ids.size > max_rows_limit
 
     where(id: matched_ids)
   end
